dc1046206288d188a8ff4636b9073f97a72f1c83
The Bit Dev <ryanvincent.capio@outlook.com>
2024-12-13 16:56:49 +0400

    Created GenericRepository

---
+++ TwoOne.Domain/Common/Repositories/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TwoOne.Domain.Common.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+    void AddAsync(T entity);
+    void UpdateAsync(T entity);
+    void DeleteAsync(T entity);
+}
---
+++ TwoOne.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TwoOne.Domain.Common.Repositories;
+
+namespace TwoOne.Persistence.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly AppDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+    
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public void AddAsync(T entity)
+    {
+         _dbSet.Add(entity);
+    }
+
+    public void UpdateAsync(T entity)
+    {
+         _dbSet.Update(entity);
+    }
+
+    public void DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
